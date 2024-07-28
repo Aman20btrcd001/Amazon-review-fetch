16e399bdf71a1771374edac3e23cf6ccf2699c81
Aman20btrcd001 <kumaraman1727@gmail.com>
2024-07-28 17:09:57 +0530

    update

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, send_file
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
